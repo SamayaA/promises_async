e0c061f66f1472a39fe47c2db2f468f1b725a99e
SamayaA <asadullaevasamaya@gmail.com>
2023-01-11 00:13:00 +0300

    fix conversion from buffer to str

--- src/__tests_/gameSavingLoader.test.js
+++ src/__tests_/gameSavingLoader.test.js
@@ -1,11 +1,15 @@
 import GameSavingLoader from '../js/GameSavingLoader';
 
-const input = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
 
 test('test GameSavingLoader', async () => {
-  const expected = await JSON.parse(input);
-  GameSavingLoader.load().then((data) => {
-    const result = JSON.parse(data);
-    expect(result).toEqual(expected);
-  });
+  const data = await GameSavingLoader.load();
+  const buffer = new Uint16Array(data);
+  let result = '';
+  let i = 0;
+  while (buffer[i]) {
+    result += String.fromCharCode(buffer[i]);
+    i++;
+  }
+  expect(result).toEqual(expected);
 });
